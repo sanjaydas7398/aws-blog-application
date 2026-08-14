fa8f8a489f23eb2b6edd3f3d5fb0ba6bb8556da2
Sanjay <sdas69101@gmail.com>
2026-08-15 01:26:52 +0530

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const blogRoutes = require("./routes/blogs");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
